a19f242c6fbf2c831f9480d00808b83ff4b5b94a
Michael Johansson <mikejoh12@gmail.com>
2024-02-06 11:11:52 -0400

    Enable successful tests for kiwipete position

--- ferris-chess-board/tests/chess_prog_pos_2.rs
+++ ferris-chess-board/tests/chess_prog_pos_2.rs
@@ -1,10 +1,5 @@
 // https://www.chessprogramming.org/Perft_Results
-// Position 2 Perft 1-4
-// Fails at n 4 for black with too few moves
-// left: 4079339
-// right: 4085603
-
-
+// Position 2. Passes n 1-5
 
 use ferris_chess_board::*;
 mod common;
@@ -31,10 +26,16 @@ fn perft_n_3() {
     assert_eq!(result, 97862);
 }
 
-#[ignore]
 #[test]
 fn perft_n_4() {
     let mut board = Board::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
     let result = perft(4, &mut board);
     assert_eq!(result, 4085603);
+}
+
+#[test]
+fn perft_n_5() {
+    let mut board = Board::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+    let result = perft(5, &mut board);
+    assert_eq!(result, 193690690);
 }
